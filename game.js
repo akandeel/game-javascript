7f7192dcb2286217f50705fa01f6c5b6ad252626
your github.com username <kandeel.a1@gmail.com>
2017-10-18 22:05:50 -0400

    console.log a message to indicate damage you did to the dragon

--- game.js
+++ game.js
@@ -9,4 +9,10 @@ $(function() {
   var youHit = Math.floor(Math.random() * 2);
   var damageThisRound = Math.floor(Math.random() * 5 + 1);
   var totalDamage = 0;
+
+  while (slaying) {
+    if (youHit){
+      console.log("Good knight You have struck the dragon and did" + damageThisRound + "damage!");
+    }
+  }
 })
